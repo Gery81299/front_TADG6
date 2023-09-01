160c26afd6d3968f7980e9817774d60dd55ad7c7
Kevin Pantoja <kevin.pantoja@unmsm.edu.pe>
2023-08-31 23:53:57 -0500

    login conectado a api rest

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,29 +1,46 @@
-import React, {useState} from "react";
-
-
+import React, { useState } from "react";
+import axios from "axios";
 
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
+    const handleLogin = async () => {
+        try {
+            console.log("1");
+            const response = await axios.post("http://127.0.0.1:9000/api/usersIntern_login", {
+                "_Correo": email,
+                "_Contraseña": password
+            });
+            console.log("2");
+            console.log(response); // No necesitas comillas alrededor de response aquí
+            if (response.data.message.includes("Credenciales válidas: Usuario existe.")) {
+                console.log("Acceso concedido");
+                window.confirm("acceso concedido");
+                // Realiza acciones después de un inicio de sesión exitoso
+            } else {
+                console.log("Acceso denegado");
+                window.confirm("contraseña o correo incorrectos");
+                // Realiza acciones cuando el inicio de sesión no es exitoso
+            }
+        } catch (error) {
+            console.error("Error en la solicitud:", error);
+        }
+    };
 
-    return(
+    return (
         <>
-        <div className="auth-form-contaniner"> 
-        <h2>LOGIN</h2>
-        <form className="login-form">
-            <label for="email">email</label>
-            <input type="email" placeholder="test@email.com" id="email" name="email"/>
+            <div className="auth-form-container">
+                <h2>LOGIN</h2>
+                <form className="login-form">
+                    <label htmlFor="email">email</label>
+                    <input type="email" placeholder="test@email.com" id="email" name="email" onChange={(e) => setEmail(e.target.value)} />
 
-            <label for="password">contraseña</label>
-            <input type="password" placeholder="********" id="password" name="password"/>
-            
-        </form>
-        <button >Ingresar</button>
-        </div>
-        
-        </>
-    )
-        
-    
-}
+                    <label htmlFor="password">contraseña</label>
+                    <input type="password" placeholder="****" id="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                </form>
+                <button onClick={handleLogin}>Ingresar</button>
+            </div>
+        </>
+    );
+};
