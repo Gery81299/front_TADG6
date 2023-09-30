cdd2b06e93071ae40f7e2f5e7d51203ffb7af266
Kevin Pantoja <kevin.pantoja@unmsm.edu.pe>
2023-09-30 00:04:43 -0500

    front mejorado

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom"; 
+import './login.css';
 
 export const Login = () => {
     const [email, setEmail] = useState('');
@@ -51,18 +52,117 @@ export const Login = () => {
     }
 
     return (
-        <>
-            <div className="auth-form-container">
-                <h2>LOGIN</h2>
-                <form className="login-form">
-                    <label htmlFor="email">email</label>
-                    <input type="email" placeholder="test@email.com" id="email" name="email" onChange={(e) => setEmail(e.target.value)} />
+        
+            
+            <section className="vh-100">
+                <div className="container-fluid h-custom">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-md-9 col-lg-6 col-xl-5">
+                        <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                        className="img-fluid"
+                        alt="Sample image"
+                        />
+                    </div>
+                    <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                        <form>
+                        <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                            <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+                            <button type="button" className="btn btn-primary btn-floating mx-1">
+                            </button>
 
-                    <label htmlFor="password">contraseña</label>
-                    <input type="password" placeholder="****" id="password" name="password" onChange={(e) => setPassword(e.target.value)} />
-                </form>
-                <button onClick={handleLogin}>Ingresar</button>
-            </div>
-        </>
+                            <button type="button" className="btn btn-primary btn-floating mx-1">
+                            </button>
+
+                            <button type="button" className="btn btn-primary btn-floating mx-1">
+                            </button>
+                        </div>
+
+                        <div className="divider d-flex align-items-center my-4">
+                            <p className="text-center fw-bold mx-3 mb-0">Or</p>
+                        </div>
+
+                        {/* Email input */}
+                        <div className="form-outline mb-4">
+                            <input
+                            type="email"
+                            id="email"
+                            className="form-control form-control-lg"
+                            placeholder="test@email.com"
+                            name="email" onChange={(e) => setEmail(e.target.value)} />
+                            <label className="form-label" htmlFor="email">
+                            Email address
+                            </label>
+                        </div>
+
+                        {/* Password input */}
+                        <div className="form-outline mb-3">
+                            <input
+                            type="password"
+                            id="password" name="password" onChange={(e) => setPassword(e.target.value)} 
+                            className="form-control form-control-lg"
+                            placeholder="****"
+                            />
+                            <label className="form-label" htmlFor="password">
+                            Password
+                            </label>
+                        </div>
+
+                        <div className="d-flex justify-content-between align-items-center">
+                            {/* Checkbox */}
+                            <div className="form-check mb-0">
+                            <input
+                                className="form-check-input me-2"
+                                type="checkbox"
+                                value=""
+                                id="form2Example3"
+                            />
+                            <label className="form-check-label" htmlFor="form2Example3">
+                                Remember me
+                            </label>
+                            </div>
+                            <a href="#!" className="text-body">
+                            Forgot password?
+                            </a>
+                        </div>
+
+                        <div className="text-center text-lg-start mt-4 pt-2">
+                            <button
+                            type="button"
+                            className="btn btn-primary btn-lg"
+                            style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}
+                            onClick={handleLogin} >
+                            Login
+                            </button>
+                            <p className="small fw-bold mt-2 pt-1 mb-0">
+                            Don't have an account? <a href="#!" className="link-danger">Register</a>
+                            </p>
+                        </div>
+
+                        </form>
+                    </div>
+                    </div>
+                </div>
+                <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+                    {/* Copyright */}
+                    <div className="text-white mb-3 mb-md-0">
+                    Copyright © 2020. All rights reserved.
+                    </div>
+                    {/* Copyright */}
+
+                
+                </div>
+                </section>
+        
+
+
+
+
+
+
+        
+
+
+
     );
 };
